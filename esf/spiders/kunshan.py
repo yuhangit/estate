e8f78699f3553d70be3e4dde728cc4f5d0089470
logan <gufengxiaoyuehan@163.com>
2018-02-28 10:42:07 +0800

    modified

--- esf/spiders/kunshan.py
+++ esf/spiders/kunshan.py
@@ -19,31 +19,60 @@ from bs4 import BeautifulSoup
 import requests
 
 
-
 class KunshanAllScrapeScripe(scrapy.spiders.CrawlSpider):
     start_urls = [ 'http://house.ks.js.cn/secondhand.asp']
     name = 'KunShanAllScrapeSpider'
     spc_reg = re.compile(r"\s+")
 
-    rules = (Rule( LinkExtractor(restrict_xpaths='//div[@class="page"]')),
+    rules = (Rule(LinkExtractor(restrict_xpaths='//div[@class="page"]')),
              Rule(LinkExtractor(restrict_xpaths='//ul[@id="xylist"]/li//a'), callback="parse_item")
              )
 
     def parse_item(self,response):
+        # agency table
         l = ItemLoader(item=AgentItem(), response=response)
         l.default_output_processor = TakeFirst()
         l.add_xpath("name", '//div[@class="sthys3"]/text()', re=r"：(\w+)")
         l.add_xpath("telephone", '//div[@class="sttelct2 sttelct"]/text()',
                     MapCompose(lambda x: "".join(x.split())))
+        l.item.setdefault("company", None)
         l.add_xpath("company", '//li[@class="st14 stb starial"]//text()')
         l.add_xpath("address", '//div[@class="xflilist"]/div[3]//text()',
                     re = r'：(\w+)')
         l.add_xpath("register_date", '//div[@class="jbfx"]/text()', re=r'登记日期：([\d/]+)')
         l.add_value("district", "昆山")
-        l.add_xpath("subdistrict",'(//div[@class="xx_xq_l200"])[2]/text()', re='区域：(\\W)*(昆山)?(\\w+)')[2]
+        l.add_xpath("subdistrict",'(//div[@class="xx_xq_l200"])[2]/text()', re='区域：(?:昆山)?(\\w+)')
 
         # housekeeping
         l.add_value("source", response.url)
+        l.add_value("project", self.settings.get("BOT_NAME"))
+        l.add_value("spider", self.name)
+        l.add_value("server", socket.gethostname())
+        l.add_value("date", datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+        yield l.load_item()
+
+        # properties table
+        l = ItemLoader(item=ScrapeItem(), response=response)
+        l.default_output_processor = TakeFirst()
+        l.add_xpath('title', '//div[@class="xxview_title"]/text()')
+        l.add_value("url", response.url)
+        l.add_xpath("price", '//div[@class="xx_xq_l200"]/span[@class="st22 '
+                             'sthuangs stb starial"]/text()')
+        l.add_xpath("address",'//div[@class="wydzleft"]/text()', MapCompose(lambda x: x.strip()),
+                    re=r'物业地址：([^\x01-\x1f]+)')
+        l.add_value("district", "昆山")
+        l.add_xpath("subdistrict", '(//div[@class="xx_xq_l200"])[2]/text()', re='区域：(?:昆山)?(\\w+)')
+        l.add_xpath("agent_name", '//div[@class="sthys3"]/text()', re=r"：(\w+)")
+        l.item.setdefault("agent_company", None)
+        l.add_xpath("agent_company", '//li[@class="st14 stb starial"]//text()')
+        l.add_xpath('agent_phone','//div[@class="sttelct2 sttelct"]/text()',
+                    MapCompose(lambda x: "".join(x.split())))
+        l.add_value('source_name', "昆山视窗")
+        l.add_value("category", "secondhose")
+        l.add_xpath("recent_activation", '//div[@class="fyfbtime"]/text()', re = '查看人次：(\\d+)')
+
+        # housekeeping
+        l.add_value("source", response.request.url)
         l.add_value("project", self.settings.get("BOT_NAME"))
         l.add_value("spider", self.name)
         l.add_value("server", socket.gethostname())
