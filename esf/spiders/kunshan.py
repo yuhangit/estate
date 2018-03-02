45037da7821d19819b376747105595c6a3462e81
logan <gufengxiaoyuehan@163.com>
2018-03-02 18:14:41 +0800

    test

--- esf/spiders/kunshan.py
+++ esf/spiders/kunshan.py
@@ -52,7 +52,7 @@ class KunshanAllScrapeScripe(scrapy.spiders.CrawlSpider):
 
         if not l.item.get("subdist_name"):
             self.logger.critical("subdsitrict name is not scrape, save response as a file")
-            f = open("html_%s.html", parse_qs(urlparse(response.url).query).get("id")[0])
+            f = open("html_%s.html" % parse_qs(urlparse(response.url).query).get("id")[0])
             f.write(response.url)
             f.close()
 
